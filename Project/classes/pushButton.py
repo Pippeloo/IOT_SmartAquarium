090952261e67f6e1f6489d936d1d0a3afc1309b8
Pippeloo <julestorfs@amps.be>
2022-04-01 15:38:34 +0200

    Added the Pushbutton class

---
+++ Project/classes/pushButton.py
@@ -0,0 +1,38 @@
+# =============
+# Jules Torfs
+# r0878800
+# All rights reserved
+# =============
+
+# import the necessary packages
+import RPi.GPIO as GPIO
+import time
+
+# ====== CLASSES ======
+class PushButton:
+    lastMillis = 0
+    lastState = False
+    # debounce time in milliseconds
+    debounce = 100
+
+    def __init__(self, pin):
+        self.pin = pin
+        # set the pin as input
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.lastState = not GPIO.input(self.pin)
+
+    def currentMillis(self):
+        return round(time.time() * 1000)
+    
+    def isPressed(self, debounceActive=False):
+        if debounceActive:
+            self.currentState = not GPIO.input(self.pin)
+            # Check if the state changes with debounce
+            if self.currentState != self.lastState:
+                if self.currentMillis() - self.lastMillis > self.debounce:
+                    self.lastState = self.currentState
+                    self.lastMillis = self.currentMillis()
+                    return self.currentState
+            return self.lastState
+        else:
+            return not GPIO.input(self.pin)
