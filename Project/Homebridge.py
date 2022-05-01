94c476bfcb14bb6b9787a04c3114e0f778e1582a
Pippeloo <julestorfs@amps.be>
2022-05-01 13:11:03 +0200

    Added final script changes for first Release version

--- Project/Homebridge.py
+++ Project/Homebridge.py
@@ -1,22 +1,44 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-import json
-import RPi.GPIO as GPIO
+# =============
+# Jules Torfs
+# r0878800
+# All rights reserved
+# =============
 
+# import the classes
 from classes.relay import Relay
 
-hostName = "localhost"
-serverPort = 6969
+# import the necessary packages
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import RPi.GPIO as GPIO
+import time
+import json
 
+# ===== FUNSTIONS =====
+def millis():
+    return round(time.time() * 1000)
+
+# ===== SETUP =====
+# Setup the pins
 RELAY_LAMP_PIN = 4
 
-json_data = {"status": "off"}
-
+# Setup the classes
 relayLamp = Relay(RELAY_LAMP_PIN)
 
+# --- Create the variables ---
+# Create variables for the webserver
+HOSTNAME = "localhost"
+SERVER_PORT = 6969
+json_data = {"status": "off"}
+
+# Create variables for the timers
+timerBusy = False
+timerStart = 0
+timeUntillLightOff = 9000 # 15 minutes
+
+# --- SETUP CLASS ---
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        global timerBusy, timerStart, timeUntillLightOff
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
@@ -26,10 +48,20 @@ class MyServer(BaseHTTPRequestHandler):
                 json_data["status"] = "on"
             else:
                 json_data["status"] = "off"
+                timerBusy = False
+
+            if timerBusy and (millis() - timerStart > timeUntillLightOff):
+                timerBusy = False
+                relayLamp.set(False)
+                json_data["status"] = "off"
+
             self.wfile.write(bytes(json.dumps(json_data), "utf-8"))
         if self.path == "/light/on":
             json_data["status"] = "on"
             relayLamp.set(True)
+            if not timerBusy:
+                timerBusy = True
+                timerStart = millis()
             self.wfile.write(bytes(json.dumps(json_data), "utf-8"))
         if self.path == "/light/off":
             json_data["status"] = "off"
@@ -39,9 +71,11 @@ class MyServer(BaseHTTPRequestHandler):
     def log_message(self, format, *args):
         pass
 
+
+# ===== MAIN =====    
 if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    webServer = HTTPServer((HOSTNAME, SERVER_PORT), MyServer)
+    print("Server started http://%s:%s" % (HOSTNAME, SERVER_PORT))
 
     try:
         webServer.serve_forever()
---
+++ Project/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python Homebridge.py & python project.py & bash ./streamCam/picam-stream.sh
