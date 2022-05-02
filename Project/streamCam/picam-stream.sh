c94acfd1c7ea656ce5a30c5ffbb0039b4f3125f3
Pippeloo <julestorfs@amps.be>
2022-05-02 15:53:06 +0200

    path mistake

--- Project/streamCam/picam-stream.sh
+++ Project/streamCam/picam-stream.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Import the configuration file
-. ./streamCam/setup.config
-
+. ./setup.config
+echo $PATH
 # Command
 echo "Starting stream..."
 libcamera-vid -n -t 0 --width $WIDTH --height $HEIGHT --framerate $FRAMERATE --bitrate $BITRATE -g $KEYFRAME -o - | ffmpeg -f lavfi -i anullsrc -c:a aac -r $FRAMERATE -i - -g $KEYFRAME -strict experimental -threads 4 -vcodec copy -map 0:a -map 1:v -b:v $BITRATE -preset ultrafast -f flv "${TWITCH_STREAM_URL}/${TWITCH_STREAM_KEY}"
