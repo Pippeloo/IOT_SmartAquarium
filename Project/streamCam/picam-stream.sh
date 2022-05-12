78aa53e2892b4c9ec8d98b1ceacf83ae1ac46950
Pippeloo <julestorfs@amps.be>
2022-05-12 11:05:49 +0200

    Fixed issue with bash script

--- Project/streamCam/picam-stream.sh
+++ Project/streamCam/picam-stream.sh
@@ -1,24 +1,4 @@
-#!/bin/bash
-# -=====- BASIC SETUP -=====-
-# Twitch Stream setup
-TWITCH_STREAM_URL=rtmp://live.twitch.tv/app/
-TWITCH_STREAM_KEY=live_408109025_naoQdtan54aDKDIYQT0UzCizHtew47
-
-# -=====- ADVANCED SETUP -=====-
-# Set width and height of output video
-WIDTH=1920
-HEIGHT=1080
-
-# Set output framerate
-FRAMERATE=30
-
-# Set keyframe spacing (must be double the framerate)
-KEYFRAME=60
-
-# Set bitrate (Twitch recommends 3500000)
-BITRATE=3500000
-
 # Command
 echo "Starting stream..."
 echo 'libcamera-vid -n -t 0 --width $WIDTH --height $HEIGHT --framerate $FRAMERATE --bitrate $BITRATE -g $KEYFRAME -o - | ffmpeg -f lavfi -i anullsrc -c:a aac -r $FRAMERATE -i - -g $KEYFRAME -strict experimental -threads 4 -vcodec copy -map 0:a -map 1:v -b:v $BITRATE -preset ultrafast -f flv "${TWITCH_STREAM_URL}/${TWITCH_STREAM_KEY}"'
-libcamera-vid -n -t 0 --width $WIDTH --height $HEIGHT --framerate $FRAMERATE --bitrate $BITRATE -g $KEYFRAME -o - | ffmpeg -f lavfi -i anullsrc -c:a aac -r $FRAMERATE -i - -g $KEYFRAME -strict experimental -threads 4 -vcodec copy -map 0:a -map 1:v -b:v $BITRATE -preset ultrafast -f flv "${TWITCH_STREAM_URL}/${TWITCH_STREAM_KEY}"
+libcamera-vid -n -t 0 --width 1920 --height 1080 --framerate 30 --bitrate 3500000 -g 60 -o - | ffmpeg -f lavfi -i anullsrc -c:a aac -r 30 -i - -g 60 -strict experimental -threads 4 -vcodec copy -map 0:a -map 1:v -b:v 3500000 -preset ultrafast -f flv "rtmp://live.twitch.tv/app/live_408109025_naoQdtan54aDKDIYQT0UzCizHtew47"
