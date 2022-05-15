666bd914401775c8dca1858db81ed428277db475
Pippeloo <julestorfs@amps.be>
2022-05-15 10:33:04 +0200

    Added vertical flip to camera

--- Project/streamCam/picam-stream.sh
+++ Project/streamCam/picam-stream.sh
@@ -5,4 +5,4 @@
 # Height -> 1080
 # Bitrate -> 3500000
 echo "Starting stream..."
-libcamera-vid -n -t 0 --width 1920 --height 1080 --framerate 30 --bitrate 3500000 -g 60 -o - | ffmpeg -f lavfi -i anullsrc -c:a aac -r 30 -i - -g 60 -strict experimental -threads 4 -vcodec copy -map 0:a -map 1:v -b:v 3500000 -preset ultrafast -f flv "rtmp://live.twitch.tv/app/live_408109025_naoQdtan54aDKDIYQT0UzCizHtew47"
+libcamera-vid -n -t 0 --width 1920 --height 1080 --framerate 30 --bitrate 3500000 -g 60 -o - | ffmpeg -f lavfi -i anullsrc -c:a aac -r 30 -i - -g 60 -strict experimental -threads 4 -vcodec copy -map 0:a --vflip -map 1:v -b:v 3500000 -preset ultrafast -f flv "rtmp://live.twitch.tv/app/live_408109025_naoQdtan54aDKDIYQT0UzCizHtew47"
